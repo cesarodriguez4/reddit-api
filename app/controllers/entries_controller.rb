140a333d7a0e4f97fcdf15898572e7c4ae598416
Cesar Rodriguez <cesarodriguez4@gmail.com>
2020-03-18 10:29:09 -0400

    Implementing Basic HTTP provider. Returns the top 25 entries in reddit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
+require 'httparty'
+require 'json'
+#  Entries controller
 class EntriesController < ApplicationController
   def top
+    url = 'https://www.reddit.com/r/all/.json?count=25'
+    response = HTTParty.get(url, { headers:
+      { 'User-Agent' => 'reddit-app:v0.0 (by /u/cesarodriguez4)' } })
+    res = JSON.parse(response.body)
+    render json: res['data']['children']
   end
 end
